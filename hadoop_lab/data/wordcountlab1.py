cd0dfdafc4cf3ef720e7157e453291d86b2cc788
ferney alberto beltran molina <Ferney.Beltran@gmail.com>
2023-02-13 23:54:21 -0500

    Add files via upload

---
+++ hadoop_lab/data/wordcountlab1.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+import re
+WORD_REGEX=re.compile(r"\b\w+\b")
+class WordCount(MRJob):
+	def mapper(self, _, line):
+		words =WORD_REGEX.findall(line)
+		for word in words:
+			yield (word.lower(),1)
+
+	def reducer (self, word, counts):
+		yield (word, sum(counts))
+
+if __name__== "__main__":
+	WordCount.run()
